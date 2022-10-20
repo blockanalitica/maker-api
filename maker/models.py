5573a7b4bf51e02b3bc1d800eb1afd58eb8b1e7e
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2022-10-20 12:05:36 +0200

    Removed MakerWalletOwner and MakerWallet

--- maker/models.py
+++ maker/models.py
@@ -566,41 +566,6 @@ class VaultOwner(TimeStampedModel):
         return self.address
 
 
-class MakerWalletOwner(TimeStampedModel):
-    name = models.CharField(max_length=256)
-    slug = AutoSlugField(populate_from="name", null=True)
-    is_whale = models.BooleanField(default=False)
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__}: name={self.name}>"
-
-    def __str__(self):
-        return self.name
-
-
-class MakerWallet(TimeStampedModel):
-    address = models.CharField(max_length=42, unique=True)
-    name = models.CharField(max_length=256, null=True)
-    owner = models.ForeignKey(
-        "MakerWalletOwner",
-        related_name="wallets",
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-    )
-    is_institution = models.BooleanField(default=False)
-
-    @property
-    def etherscan_url(self):
-        return "https://etherscan.io/address/{}".format(self.address)
-
-    def __str__(self):
-        return self.name or self.address
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__}: address={self.address}, name={self.name}>"
-
-
 class VaultsLiquidationHistory(TimeStampedModel):
     ilk = models.CharField(max_length=32)
     drop = models.IntegerField()
