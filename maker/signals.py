15de7c359382cc4688d1cdb18e84d435dd29ad9e
0xCommanderKeen <91289621+0xCommanderKeen@users.noreply.github.com>
2022-11-22 16:03:33 +0100

    add RETH price

--- maker/signals.py
+++ maker/signals.py
@@ -13,6 +13,11 @@ from maker.models import Asset
 @receiver(post_save, sender="maker.TokenPriceHistory")
 def chainlink_price_change(sender, instance, created, **kwards):
     if instance.price > 0:
+        if instance.underlying_address == "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2":
+            Asset.objects.filter(
+                address="0xae78736cd615f374d3085123a210448e74fc6393"
+            ).update(price=instance.price, modified=datetime.now())
+
         Asset.objects.filter(address=instance.underlying_address).update(
             price=instance.price, modified=datetime.now()
         )
