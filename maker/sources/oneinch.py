23c687cf9d2c470041bd7344f8e0cf72c0af7228
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2023-01-18 17:33:36 +0100

    Increase backoff factor for 1inch

--- maker/sources/oneinch.py
+++ maker/sources/oneinch.py
@@ -17,6 +17,7 @@ def get_oneinch_quote(from_token_address, to_token_address, amount):
     )
     return retry_get_json(
         url,
+        backoff_factor=1,
         status_forcelist=(400, 429, 500, 502, 503, 504),
         respect_retry_after_header=False,
     )
