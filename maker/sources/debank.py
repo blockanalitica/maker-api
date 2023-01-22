18725560b2ed64465c4aa41c441a4fe614c058d3
0xCommanderKeen <91289621+0xCommanderKeen@users.noreply.github.com>
2023-01-22 12:42:03 +0100

    license

--- maker/sources/debank.py
+++ maker/sources/debank.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: © 2022 Dai Foundation <www.daifoundation.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from django.conf import settings
 
 from maker.utils.http import retry_get_json
@@ -12,24 +16,3 @@ def fetch_user_token_list(wallet_address):
         headers={"AccessKey": settings.DEBANK_API_KEY},
     )
     return data
-
-
-"""
- {'id': 'eth',
-  'chain': 'eth',
-  'name': 'ETH',
-  'symbol': 'ETH',
-  'display_symbol': None,
-  'optimized_symbol': 'ETH',
-  'decimals': 18,
-  'logo_url': 'https://static.debank.com/image/token/logo_url/eth/935ae4e4d1d12d59a99717a24f2540b5.png',
-  'protocol_id': '',
-  'price': 1624.79,
-  'is_verified': True,
-  'is_core': True,
-  'is_wallet': True,
-  'time_at': 1483200000.0,
-  'amount': 3442.2988255541986,
-  'raw_amount': 3442298825554198670666,
-  'raw_amount_hex_str': '0xba9b7e1fc2be5a2d4a'}
-"""
