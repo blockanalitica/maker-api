fff8ee73c69517588d6008180bf12aaa6fbd2378
Twigmaester <cehic.elvis1@gmail.com>
2023-10-25 11:49:55 +0200

    feat: add urn event state model and script

--- maker/sources/cortex.py
+++ maker/sources/cortex.py
@@ -33,3 +33,19 @@ def fetch_cortext_ilk_vaults(ilk):
 
         for vault in data["results"]:
             yield vault
+
+
+def fetch_cortex_urn_states(block_number):
+    next_url = (
+        f"{settings.BLOCKANALITICA_CORTEX_URL}/"
+        f"api/v1/maker/vaults/events?block_number_gt={block_number}&page_size=10000"
+    )
+    while next_url is not None:
+        data = _cortex_get(next_url)
+        if data["next_page_uri"]:
+            next_url = data["next_page_uri"]
+        else:
+            next_url = None
+
+        for vault in data["results"]:
+            yield vault
