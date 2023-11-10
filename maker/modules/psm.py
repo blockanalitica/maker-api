901ab3be42c53ddc72c0fdedcf593e3017788abd
Twigmaester <cehic.elvis1@gmail.com>
2023-11-10 09:25:47 +0100

    fix: psm balances

--- maker/modules/psm.py
+++ maker/modules/psm.py
@@ -9,7 +9,7 @@ from django.db import connection
 from eth_utils import to_bytes
 
 from maker.constants import MCD_VAT_CONTRACT_ADDRESS
-from maker.models import Block, Ilk, PSMDAISupply
+from maker.models import Block, Ilk, PSMDAISupply, UrnEventState
 from maker.utils.blockchain.chain import Blockchain
 from maker.utils.views import fetch_all
 
@@ -25,26 +25,32 @@ def claculate_and_save_psm_dai_supply():
         current_hour = datetime.now().replace(minute=0, second=0, microsecond=0)
         try:
             latest = PSMDAISupply.objects.filter(ilk=ilk).latest()
+            from_dt = latest.datetime
+            total_supply = Decimal(
+                str(
+                    UrnEventState.objects.filter(ilk=ilk, datetime__lte=from_dt)
+                    .latest()
+                    .art
+                )
+            ) / Decimal("1e18")
         except PSMDAISupply.DoesNotExist:
             total_supply = Decimal("0")
             from_dt = datetime(2000, 1, 1)
-        else:
-            from_dt = latest.datetime + timedelta(hours=1)
-            total_supply = latest.total_supply
 
         # Don't select events from current hour, as if we do, events that will come
         # later on in the same current hour, won't be counted towards it and will
         # be skipped causing wrong numbers
         sql = """
-            SELECT
+            SELECT DISTINCT on (dt)
                 DATE_TRUNC('hour', datetime) as dt
-                , SUM(dart/1e18 * rate/1e27) as amount
+                , datetime
+                , art/1e18 as total_supply
             FROM maker_urneventstate
             WHERE ilk = %s
                 AND datetime > %s
                 AND datetime < %s
-            GROUP BY 1
-            ORDER BY 1
+            GROUP BY 1, 2, 3
+            ORDER BY 1 DESC, 2 DESC
         """
         with connection.cursor() as cursor:
             cursor.execute(sql, [ilk, from_dt, current_hour])
@@ -52,22 +58,23 @@ def claculate_and_save_psm_dai_supply():
 
         bulk_create = []
         for event in events:
-            total_supply += event["amount"]
+            supply_change = event["total_supply"] - total_supply
+            total_supply = event["total_supply"]
             bulk_create.append(
                 PSMDAISupply(
                     ilk=ilk,
                     datetime=event["dt"],
                     timestamp=event["dt"].timestamp(),
                     total_supply=total_supply,
-                    supply_change=event["amount"],
+                    supply_change=supply_change,
                 )
             )
             if len(bulk_create) == 500:
-                PSMDAISupply.objects.bulk_create(bulk_create)
+                PSMDAISupply.objects.bulk_create(bulk_create, ignore_conflicts=True)
                 bulk_create = []
 
         if len(bulk_create) > 0:
-            PSMDAISupply.objects.bulk_create(bulk_create)
+            PSMDAISupply.objects.bulk_create(bulk_create, ignore_conflicts=True)
 
 
 def calculate_and_save_psm_dai_supply_for_rwa(block_number="latest"):
