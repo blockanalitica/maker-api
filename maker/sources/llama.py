30f29df82a1658293ec22f7462a33c330fb812aa
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2024-12-22 20:27:29 +0100

    fix: defillama fix

--- maker/sources/llama.py
+++ maker/sources/llama.py
@@ -8,12 +8,12 @@ LLAMA_COINS_API_URL = "https://coins.llama.fi/"
 
 
 def fetch_current_price(coins):
-    url = "prices/current/{}/".format(",".join(coins))
+    url = "prices/current/{}".format(",".join(coins))
     data = retry_get_json("{}{}".format(LLAMA_COINS_API_URL, url))
     return data["coins"]
 
 
 def fetch_price_for_timestamp(timestamp, coins):
-    url = "prices/historical/{}/{}/".format(timestamp, ",".join(coins))
+    url = "prices/historical/{}/{}".format(timestamp, ",".join(coins))
     data = retry_get_json("{}{}".format(LLAMA_COINS_API_URL, url))
     return data["coins"]
